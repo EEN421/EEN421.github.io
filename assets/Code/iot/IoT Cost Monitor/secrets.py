5e8cf4d0a640a983445fd7bef310210496316c6c
Ian Hanley <118940205+EEN421@users.noreply.github.com>
2023-07-06 01:07:59 -0400

    Code Push

---
+++ assets/Code/iot/IoT Cost Monitor/secrets.py
@@ -0,0 +1,8 @@
+secrets = {
+  "ssid" : "Your-WiFi-SSID",
+  "password" : "Your-WiFi-PSK",
+  "appId": "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
+  "clientSecret": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  "tenant": "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
+  "subscription": "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+}
